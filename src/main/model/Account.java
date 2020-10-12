d829ea9128351baad8744a7adaf1108995289add
InstantRobin <cryerrobin@yahoo.com>
2020-10-12 03:03:28 -0700

    Added name variable to Account Created LogEntry class

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -6,8 +6,10 @@ public class Account {
     // TODO: Add Currency
     // the balance of the account, in cents
     private int balance;
+    private String name;
 
-    public Account(int balance) {
+    public Account(String name, int balance) {
+        this.name = name;
         this.balance = balance; // initial balance
     }
 
@@ -17,6 +19,10 @@ public class Account {
 
     public void subValue(int value) {
         this.balance -= value;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getBalance() {
---
+++ src/main/model/LogEntry.java
@@ -0,0 +1,10 @@
+package model;
+// Represents a log entry of an account value change
+
+public class LogEntry {
+
+    public LogEntry(){
+
+    }
+
+}
