a715d00f88c9c0c80396c422a2954bf8cb9219bc
InstantRobin <cryerrobin@yahoo.com>
2020-10-12 03:56:34 -0700

    Added history of logEntries to Account, unsure if will keep this formatting.

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -1,26 +1,36 @@
 package model;
 // Represents a place where money is stored
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
 
     // TODO: Add Currency
-    // TODO: Record Log Entry ?
+    // TODO: Decide how to store Log Entries
 
     // the balance of the account, in cents
     private int balance;
     private String name;
+    private List<LogEntry> history = new ArrayList<LogEntry>();
 
     public Account(String name, int balance) {
         this.name = name;
         this.balance = balance; // initial balance
     }
 
-    public void addValue(int value) {
-        this.balance += value;
+    public void logEvent(int value, boolean add, String date) {
+        history.add(new LogEntry(this, value,true, date));
     }
 
-    public void subValue(int value) {
+    public void addValue(int value, String date) {
+        this.balance += value;
+        history.add(new LogEntry(this, value,true, date));
+    }
+
+    public void subValue(int value, String date) {
         this.balance -= value;
+        history.add(new LogEntry(this, value,false, date));
     }
 
     public String getName() {
--- src/test/model/AccountTest.java
+++ src/test/model/AccountTest.java
@@ -8,29 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class AccountTest {
 
     Account acc = new Account("test",0);
+    String date = "2020-10-12";
 
     @BeforeEach
     public void beforeEach(){
-        acc.subValue(acc.getBalance());
+        acc.subValue(acc.getBalance(), date);
     }
 
     @Test
     public void addValueTest(){
-        acc.addValue(1000);
+        acc.addValue(1000, date);
         assertEquals(1000,acc.getBalance());
     }
 
     @Test
     public void subValueTest(){
-        acc.addValue(1000);
-        acc.subValue(500);
+        acc.addValue(1000, date);
+        acc.subValue(500, date);
         assertEquals(500,acc.getBalance());
     }
 
     @Test
     public void subValueNegativeTest(){
-        acc.addValue(500);
-        acc.subValue(1000);
+        acc.addValue(500, date);
+        acc.subValue(1000, date);
         assertEquals(-500,acc.getBalance());
     }
 }
