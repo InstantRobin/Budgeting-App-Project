37c8a2ffbdb1055f99d5170e015bbd638aa8a0fb
InstantRobin <cryerrobin@yahoo.com>
2020-10-13 02:25:03 -0700

    Added documentation for, streamlined AccountTest

--- src/test/model/AccountTest.java
+++ src/test/model/AccountTest.java
@@ -3,6 +3,7 @@ package model;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sun.rmi.runtime.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,12 +28,16 @@ class AccountTest {
         }
     }
 
+    public LogEntry newLogEntry(int val, Boolean add){
+        return (new LogEntry(acc,val,add,date));
+    }
+
     @Test
     public void addValueTest(){ //Checks if addValue adds value, logs properly
         acc.addValue(1000, date);
 
         List<LogEntry> result = new ArrayList<>();
-        result.add(new LogEntry(acc,1000,true, date));
+        result.add(newLogEntry(1000,true));
 
         assertEquals(1000,acc.getBalance());
         compareValues(acc,result);
@@ -44,43 +49,43 @@ class AccountTest {
         acc.subValue(500, date);
 
         List<LogEntry> result = new ArrayList<>();
-        result.add(new LogEntry(acc,1000,true, date));
-        result.add(new LogEntry(acc,500,false, date));
+        result.add(newLogEntry(1000,true));
+        result.add(newLogEntry(500,false));
 
         assertEquals(500,acc.getBalance());
         compareValues(acc,result);
     }
 
     @Test
-    public void subValueNegativeTest(){
+    public void subValueNegativeTest(){ //Checks if subvalue can set to negative amount, logs properly
         acc.addValue(500, date);
         acc.subValue(1000, date);
 
         List<LogEntry> result = new ArrayList<>();
-        result.add(new LogEntry(acc,500,true, date));
-        result.add(new LogEntry(acc,1000,false, date));
+        result.add(newLogEntry(500,true));
+        result.add(newLogEntry(1000,false));
 
         assertEquals(-500,acc.getBalance());
         compareValues(acc,result);
     }
 
     @Test
-    public void logEventTest(){
+    public void logEventTest(){ //Checks if logEvent works properly with one event
         acc.logEvent(500,true, date);
         List<LogEntry> result = new ArrayList<>();
-        result.add(new LogEntry(acc,500,true, date));
+        result.add(newLogEntry(500,true));
 
         compareValues(acc,result);
     }
 
     @Test
-    public void logEventTestMult(){
+    public void logEventTestMult(){ //Checks if logEvent works properly with multiple events
         acc.logEvent(700,true,date);
         acc.logEvent(900,false,date);
 
         List<LogEntry> result = new ArrayList<>();
-        result.add(new LogEntry(acc,700,true, date));
-        result.add(new LogEntry(acc,900,false, date));
+        result.add(newLogEntry(700,true));
+        result.add(newLogEntry(900,false));
 
         compareValues(acc,result);
     }
