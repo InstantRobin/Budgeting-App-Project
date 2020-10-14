645c529b1b4a6c139af54d62302a05ae227a0591
InstantRobin <cryerrobin@yahoo.com>
2020-10-14 02:28:39 -0700

    Added Manage Documentation

--- src/main/ui/Manage.java
+++ src/main/ui/Manage.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// TODO: Restructure Heavily
 public class Manage {
 
     private List<Account> accounts = new ArrayList<>();
@@ -32,7 +31,7 @@ public class Manage {
         }
     }
 
-    // Gets User Action
+    // Presents users with option,s gets User Action
     private int getChoice() {
         System.out.print("Enter the corresponding number of desired action:\n"
                 + "1)View Account Balance\n"
@@ -46,6 +45,7 @@ public class Manage {
         return sc.nextInt();
     }
 
+    // Calls corresponding function to user choice as documented in getChoice()
     private void doChoice(int choice) {
         switch (choice) {
             case 1:
@@ -58,19 +58,17 @@ public class Manage {
                 withdraw(getAcc(),getVal(),getDate());
                 break;
             case 4:
-                System.out.println("First select source, then select destination");
-                transfer(getAcc(),getAcc(),getVal(),getDate());
+                transferStep(); // transfer requires 2 acc's, so needs more complicated fn
                 break;
-                //TODO
             case 5:
                 makeAccount(getName(),getVal());
                 break;
         }
     }
 
-    private void makeAccount(String name, int val) {
-        accounts.add(new Account(name,val));
-    }
+    //*******************//
+    //GET USER INPUT FNS//
+    //*****************//
 
     // Presents user with list of accounts, takes user input, returns selected account
     private Account getAcc() {
@@ -80,17 +78,18 @@ public class Manage {
         for (int a = 0; a < accounts.size(); a++) {
             System.out.println((a + 1) + ") " + accounts.get(a).getName());
         }
+
         int acc = sc.nextInt();
         return accounts.get(acc - 1);
     }
 
+    // Gets int value from User
     private int getVal() {
         System.out.println("Enter desired amount:");
-
         return (int)(sc.nextFloat() * 100);
-
     }
 
+    // Gets LocalDate date from user, separate input for year, month, date
     private LocalDate getDate() {
         System.out.println("Enter date of action in number form:");
         System.out.println("Year:");
@@ -103,10 +102,15 @@ public class Manage {
         return LocalDate.of(year,month,day);
     }
 
+    // Gets string name from user
     private String getName() {
         System.out.println("Enter name:");
         return sc.next();
     }
+
+    //******************//
+    //CHOICE-ACTION FNS//
+    //****************//
 
     private void viewBalance(Account acc) {
         System.out.println(acc.getStringBalance());
@@ -120,16 +124,20 @@ public class Manage {
         acc.subValue(val,date);
     }
 
+    // Intermediate step required bc fn takes two accounts, extra command needed to reduce vagueness
+    private void transferStep() {
+        System.out.println("First select source, then select destination");
+        transfer(getAcc(),getAcc(),getVal(),getDate());
+    }
+
+    // Does actual transfer process
     private void transfer(Account acc1, Account acc2, int val, LocalDate date) {
         withdraw(acc1,val,date);
         deposit(acc2,val,date);
     }
 
-    private void logAction(Account acc, int val, String date) {
-        //stub
-    }
-
-    private void returnHistory() {
-        //stub
+    // Makes new Account w/ given name, initial value
+    private void makeAccount(String name, int val) {
+        accounts.add(new Account(name,val));
     }
 }
