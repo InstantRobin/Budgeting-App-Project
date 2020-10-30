fb1eac10fc9039375553b8a9d88b0665041563f9
InstantRobin <cryerrobin@yahoo.com>
2020-10-30 00:18:38 -0700

    Created jsonReaderTest, made changes to unify reader and writer tests, also included history for both

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -7,70 +7,58 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class JsonWriterTest extends JsonTest{
+public class JsonReaderTest extends JsonTest{
 
-    // from CPSC 210 EdX JsonSerializationDemo
     @Test
-    void testWriterInvalidFile() {
+    void testReaderInvalidFile() {
         try {
-            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
-            writer.open();
-            fail("IOException was expected");
-        } catch (IOException e) {
+            JsonReader reader = new JsonReader("./data/FileNotFound.json");
+            reader.read();
+            fail("FileNotFoundException was expected");
+        } catch (IOException e){
             // pass
         }
     }
 
     @Test
-    public void jsonWriterTestEmpty() {
+    public void jsonReaderTestEmpty() {
         try {
-            String dest = "./data/testWriterEmpty.json";
+            String dest = "./data/testReaderEmpty.json";
             List<Account> accountList = new ArrayList<>();
-
-            JsonWriter jsonWriter = new JsonWriter(dest);
-            jsonWriter.open();
-            jsonWriter.write(accountList);
-            jsonWriter.close();
 
             JsonReader jsonReader = new JsonReader(dest);
             List<Account> result = jsonReader.read();
             assertEquals(accountList,result);
 
-        } catch (FileNotFoundException e){
-            fail("Unexpected FileNotFoundException");
         } catch (IOException e) {
             fail("Unexpected IOException");
         }
     }
 
     @Test
-    public void jsonWriterTestGeneral() {
+    public void jsonReaderTestGeneral() {
         try {
             setAccounts();
-            String dest = "./data/testWriterGeneral.json";
+            String dest = "./data/testReaderGeneral.json";
             List<Account> accountList = new ArrayList<>();
-            accountList.add(account1);
-            accountList.add(account2);
 
-            JsonWriter jsonWriter = new JsonWriter(dest);
-            jsonWriter.open();
-            jsonWriter.write(accountList);
-            jsonWriter.close();
+            accountList.add(account2);
+            accountList.add(account1);
 
             JsonReader jsonReader = new JsonReader(dest);
             List<Account> result = jsonReader.read();
             checkAccount(accountList,result);
 
-        } catch (FileNotFoundException e){
-            fail("Unexpected FileNotFoundException");
         } catch (IOException e) {
             fail("Unexpected IOException");
         }
     }
+
 }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,17 +1,25 @@
 package persistence;
 // based on CPSC 210 EdX JsonSerializationDemo
 
-import model.Account;
-import model.Currency;
-import model.History;
-import model.LogEntry;
+import model.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class JsonTest {
+    Account account2 = new Account("test2",543, TestDefaults.USD);
+    Account account1 = new Account("test1",10, TestDefaults.USD);
+
+    public void setAccounts() {
+        account2.logEvent(50, LocalDate.of(2020, 10, 29));
+        account2.logEvent(80, LocalDate.of(2020, 11, 19));
+        account1.logEvent(-60, LocalDate.of(2019, 9, 15));
+        account1.logEvent(230, LocalDate.of(2020, 2, 12));
+    }
+
     public static void checkAccount(List<Account> expected, List<Account> result) {
         for (int i = 0; i < expected.size(); i++) {
             Account exp = expected.get(i);
@@ -31,7 +39,7 @@ public class JsonTest {
 
     private static void compareHistory(History expected, History result) {
         if (expected.size() != result.size()) {
-            fail("Histories do not match");
+            fail("History sizes do not match");
         } else {
             for (int i = 0; i < expected.size(); i++) {
                 LogEntry expEntry = expected.get(i);
