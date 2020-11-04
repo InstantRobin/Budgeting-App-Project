9112b215d8e65bbc77576861945daeaededd7a6a
InstantRobin <cryerrobin@yahoo.com>
2020-11-04 03:23:40 -0800

    Created Transfer tests for ActionsTest

--- src/test/model/ActionsTest.java
+++ src/test/model/ActionsTest.java
@@ -18,7 +18,7 @@ public class ActionsTest extends TestDefaults{
     public void beforeEach() {
         acc1 = new Account("acc1",5000,USD);
         acc2 = new Account("acc2",1000,USD);
-        acc3 = new Account("acc2",1000,RMB);
+        acc3 = new Account("acc3",2000,RMB);
     }
 
     @Test
@@ -26,6 +26,7 @@ public class ActionsTest extends TestDefaults{
         deposit(acc1,500,date);
         assertEquals(5500, acc1.getBalance());
 
+        history.add(new LogEntry(acc1,500,5500,date));
         assertEquals(history,acc1.getHistory());
     }
 
@@ -33,6 +34,9 @@ public class ActionsTest extends TestDefaults{
     public void withdrawTest() {
         withdraw(acc1,500,date);
         assertEquals(4500, acc1.getBalance());
+
+        history.add(new LogEntry(acc1,-500,4500,date));
+        assertEquals(history,acc1.getHistory());
     }
 
     @Test
@@ -40,5 +44,42 @@ public class ActionsTest extends TestDefaults{
         transfer(acc1, acc2, 500,date);
         assertEquals(4500, acc1.getBalance());
         assertEquals(1500, acc2.getBalance());
+
+        history.add(new LogEntry(acc1,-500,4500,date));
+        assertEquals(history,acc1.getHistory());
+
+        history = new History();
+        history.add(new LogEntry(acc1,500,1500,date));
+        assertEquals(history,acc2.getHistory());
+    }
+
+    @Test
+    public void transferTestDifferentCurrencyFromUSD() {
+        transfer(acc1, acc3, 500,date);
+        int newAmount = (int) (500 / RMB.getExchangeRateUSD());
+        assertEquals(4500, acc1.getBalance());
+        assertEquals(2000 + newAmount , acc3.getBalance());
+
+        history.add(new LogEntry(acc1,-500,4500,date));
+        assertEquals(history,acc1.getHistory());
+
+        history = new History();
+        history.add(new LogEntry(acc3,newAmount,2000 + newAmount,date));
+        assertEquals(history,acc3.getHistory());
+    }
+
+    @Test
+    public void transferTestDifferentCurrencyToUSD() {
+        transfer(acc3, acc1, 500,date);
+        int newAmount = (int) (500 * RMB.getExchangeRateUSD());
+        assertEquals(5000 + newAmount, acc1.getBalance());
+        assertEquals(1500, acc3.getBalance());
+
+        history.add(new LogEntry(acc1,newAmount,5000 + newAmount,date));
+        assertEquals(history,acc1.getHistory());
+
+        history = new History();
+        history.add(new LogEntry(acc3,-500,1500,date));
+        assertEquals(history,acc3.getHistory());
     }
 }
