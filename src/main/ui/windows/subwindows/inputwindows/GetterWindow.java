3c449c07192ec9ae82ab1c63e3da30127729ddd6
InstantRobin <cryerrobin@yahoo.com>
2020-11-22 02:56:07 -0800

    Moved getGenericInput into GetterWindow, deleted InputWindow to reduce redundancy

--- src/main/ui/windows/subwindows/inputwindows/GetterWindow.java
+++ src/main/ui/windows/subwindows/inputwindows/GetterWindow.java
@@ -2,20 +2,46 @@ package ui.windows.subwindows.inputwindows;
 
 import model.Account;
 import ui.windows.Home;
+import ui.windows.SubWindow;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
 // Represents a window used to get user input
-public abstract class GetterWindow extends InputWindow {
+public abstract class GetterWindow extends SubWindow {
 
     protected Account acc;
+    protected JTextArea textArea;
 
     public GetterWindow(Container container, Home home) {
         super(container, home);
     }
 
+    // EFFECTS: Creates a window like so:
+    /*
+            str |     | Submit
+     */
+    //          The user can type in the white box, and Submit is a button to do so
+    //          Sets the textArea variable so it can be accessed by subclasses
+    //          Calls the given function
+    protected void getGenericInput(String str, Runnable rn) {
+        reset();
+
+        JTextArea message = new JTextArea(str);
+        setClearUneditableTextArea(message);
+        JTextArea inputArea = new JTextArea(1,5);
+        JButton submit = new JButton("Submit");
+
+        container.add(message);
+        container.add(inputArea);
+        container.add(submit);
+
+        textArea = (JTextArea)container.getComponent(1);
+        submit.addActionListener(e -> rn.run());
+    }
+
+    // Prompts user for an Account, sets acc to that account, runs the given fn
     protected void getAccount(Runnable fn) {
         reset();
         List<Account> accounts = manager.getAccounts();
@@ -32,4 +58,5 @@ public abstract class GetterWindow extends InputWindow {
             });
         }
     }
+
 }
