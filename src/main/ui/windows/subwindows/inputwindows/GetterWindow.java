009ca46eab0c788531851496ff94e7f9aae82f46
InstantRobin <cryerrobin@yahoo.com>
2020-11-22 00:41:26 -0800

    Moved MoneyInput implementation into GetterWindow as it was only used there

--- src/main/ui/windows/subwindows/inputwindows/GetterWindow.java
+++ src/main/ui/windows/subwindows/inputwindows/GetterWindow.java
@@ -6,6 +6,7 @@ import ui.windows.Home;
 import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public abstract class GetterWindow extends InputWindow {
 
@@ -17,15 +18,17 @@ public abstract class GetterWindow extends InputWindow {
         super(container, home);
     }
 
+    // EFFECTS: Loads an Input Window, when submit is pressed, takes the TextArea value and verifies it as a Double
+    //          Initializes function chain where windows are loaded to get user input, past input is passed along
+    //          By end of function chain, assuming good user input, will deposit inputted amount into a given acct
     protected void getInt(Runnable fn) {
-        MoneyInput moneyInput = new MoneyInput(container,home);
-        moneyInput.updateGUI();
+        getGenericInput("Input Amount: ");
 
         JButton button = (JButton)container.getComponent(2);
         JTextArea textArea = (JTextArea)container.getComponent(1);
         button.addActionListener(e -> {
             try {
-                this.val = moneyInput.verifyVal(textArea.getText());
+                this.val = verifyVal(textArea.getText());
                 fn.run();
             } catch (NumberFormatException exception) {
                 showMessageWindow("Unrecognized number, please try again");
@@ -33,6 +36,15 @@ public abstract class GetterWindow extends InputWindow {
                 showMessageWindow("Negative number not allowed, please try again");
             }
         });
+    }
+
+    public int verifyVal(String str) throws NumberFormatException, NegativeValueException {
+        double val;
+        val = Double.parseDouble(str);
+        if (val <= 0) {
+            throw new NegativeValueException();
+        }
+        return (int)(val * 100);
     }
 
     protected void getAccount(Runnable fn) {
@@ -49,15 +61,22 @@ public abstract class GetterWindow extends InputWindow {
         }
     }
 
+
+    // EFFECTS: Loads an Input Window, when submit is pressed, takes the TextArea value and verifies it as a date
+    //              if is, passes it and all past inputs into makeDeposit
+    //              if not, throws error screen, returns user to home
     protected void getDate(Runnable fn) {
-        DateInput dateInput = new DateInput(container,home);
-        dateInput.updateGUI();
+        getGenericInput("Enter Date As YYYY-MM-DD:");
 
         JButton button = (JButton)container.getComponent(2);
         JTextArea textArea = (JTextArea)container.getComponent(1);
         button.addActionListener(e -> {
-            this.date = dateInput.verifyDate(textArea.getText());
-            fn.run();
+            try {
+                this.date = LocalDate.parse(textArea.getText());
+                fn.run();
+            } catch (DateTimeParseException exception) {
+                showMessageWindow("Unrecognized Date, please try again");
+            }
         });
     }
 }
