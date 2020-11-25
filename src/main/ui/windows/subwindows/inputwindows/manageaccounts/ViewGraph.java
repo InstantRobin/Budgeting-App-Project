b222a38c7d943db0dfdfea856b6403eabaf24470
InstantRobin <cryerrobin@yahoo.com>
2020-11-25 03:25:10 -0800

    Added getInitBal to ViewGraph to fix the graph being off by the initial value of the account

--- src/main/ui/windows/subwindows/inputwindows/manageaccounts/ViewGraph.java
+++ src/main/ui/windows/subwindows/inputwindows/manageaccounts/ViewGraph.java
@@ -52,15 +52,30 @@ public class ViewGraph extends InputWindow {
     // EFFECTS: Builds collection of dates and corresponding totals, from acc history
     private TimeSeriesCollection<String> getTimeSeriesCollection() {
         History data = MoveMoneyFunctions.buildData(acc);
+
+        int dif = getInitBal();
+
         TimeSeries<String> timeSeries = new TimeSeries<>("Data");
         for (LogEntry entry : data) {
             Day day = new Day(entry.getDay(),entry.getMonth(),entry.getYear());
-            timeSeries.add(day, (double) entry.getTotal() / 100);
+            timeSeries.add(day, (double)(entry.getTotal() + dif) / 100);
         }
 
         TimeSeriesCollection<String> collection = new TimeSeriesCollection<>();
         collection.addSeries(timeSeries);
         return collection;
+    }
+
+    // EFFECTS: Returns the initial value of acc
+    // This needs to be separate as an account's initial balance is technically never deposited, as is not
+    //      stored so dif is equal to that amount, as calculated by subtracting the total of all past actions
+    // This is probably not great design, should really be overhauled at some point
+    private int getInitBal() {
+        int sum = 0;
+        for (LogEntry entry: acc.getHistory()) {
+            sum += entry.getVal();
+        }
+        return acc.getBalance() - sum;
     }
 
 }
